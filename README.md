4042d865c6fc67052822651c3889d48e0462a068
Genisson Gomes <genissongomes@gmail.com>
2021-03-27 12:31:45 -0300

    finalizando o projeto

---
+++ README.md
@@ -0,0 +1,4 @@
+## Projeto Calculadora de gorjeta
+projeto feito com o intuito de praticar sobre o assunto componentes de interface.
+prejeto foi realizado no curso de Desenvolvimento android do prof Jamilton Damasceno.
+
--- app/src/main/java/com/genisson/calculadoradegorjeta/MainActivity.kt
+++ app/src/main/java/com/genisson/calculadoradegorjeta/MainActivity.kt
@@ -1,11 +1,58 @@
+
 package com.genisson.calculadoradegorjeta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputValor: TextInputEditText
+    private lateinit var textPorcentagem: TextView
+    private lateinit var textGorjeta: TextView
+    private lateinit var textTotal: TextView
+    private lateinit var seekBarGorjeta:SeekBar
+    private var porcentagem: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inputValor = findViewById(R.id.inputValor)
+        textPorcentagem= findViewById(R.id.textPorcentagem)
+        textGorjeta = findViewById(R.id.textGorjeta)
+        textTotal = findViewById(R.id.textTotal)
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta)
+        //Adicionar listenr SeekBar
+        seekBarGorjeta.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                porcentagem = progress.toDouble()
+                textPorcentagem.text = "${Math.round(porcentagem)}%"
+                calcular()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
     }
+    fun calcular(){
+        try {
+            val valor: String = inputValor.text.toString()
+            val valorDigtado = valor.toDouble()
+            val gorjeta: Double =  valorDigtado * (porcentagem / 100)
+            val total: Double = gorjeta + valorDigtado
+            textGorjeta.text = "R$ ${"%.2f".format(gorjeta)}"
+            textTotal.text = "R$ ${"%.2f".format(total)}"
+        }catch (err: Error){
+            err.message
+        }
+
+    }
+
 }
